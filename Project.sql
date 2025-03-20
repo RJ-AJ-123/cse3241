2d587f71386296c5e9f8b4c585873047db3fa3a2
Yujie Yang <yang.6467@osu.edu>
2025-03-20 15:31:10 -0400

    update administrator

--- Project.sql
+++ Project.sql
@@ -5,6 +5,28 @@ CREATE table IF NOT EXISTS Administrators (
     ContactNumber TEXT
 );
 
+CREATE table  IF NOT EXISTS Publishers(
+    PublisherName TEXT PRIMARY KEY,
+    Email TEXT,
+    Address TEXT,
+    ContactNumber TEXT
+);
+
+CREATE table if not exists Books(
+    ISBN TEXT Primary key,
+    Title TEXT NOT NULL,
+    Author TEXT,
+    PublisherName TEXT,
+    Year INTEGER,
+    Price REAL,
+    Category TEXT,
+    StockThreshold INTEGER,
+    FOREIGN KEY (PublisherName) REFERENCES Publishers (publishername)
+        on update CASCADE
+        on delete set null
+);
+
+
 INSERT OR REPLACE INTO Administrators (AdminID, Email, Name, ContactNumber)
 VALUES
   (1, 'admin1@bookstore.com', 'Alice Admin', '123-456-7890'),
