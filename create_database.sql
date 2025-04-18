2bc1649dc018afd53ab072f86f6a6a5ee783c547
aa77733 <liyuang789@gmail.com>
2025-04-18 17:30:14 -0400

    create_database.sql

--- create_database.sql
+++ create_database.sql
@@ -1,1 +1,11 @@
 
+CREATE INDEX idx_customer_id ON Customers(CustomerID);
+CREATE INDEX idx_books_isbn ON Books(ISBN);
+CREATE INDEX idx_purchase_customer_id ON Purchases(CustomerID);
+CREATE INDEX idx_purchasedetails_purchaseid ON PurchaseDetails(PurchaseID);
+CREATE INDEX idx_purchasedetails_isbn ON PurchaseDetails(ISBN);
+CREATE INDEX idx_reviews_customerid ON Reviews(CustomerID);
+CREATE INDEX idx_reviews_isbn ON Reviews(ISBN);
+CREATE INDEX idx_promotions_isbn ON Promotions(ISBN);
+CREATE INDEX idx_reorders_isbn ON Reorders(ISBN);
+CREATE INDEX idx_reorders_adminid ON Reorders(AdminID);
