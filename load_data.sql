9d5c711d004f5eebd2d1c040075360ae49134418
aa77733 <liyuang789@gmail.com>
2025-04-18 20:21:56 -0400

    Add files via upload

---
+++ load_data.sql
@@ -0,0 +1,49 @@
+-- Inserting data into PUBLISHERS table
+
+-- (INSERT INTO PUBLISHERS ...)
+
+
+-- Inserting data into BOOKS table
+
+-- (INSERT INTO BOOKS ...)
+
+
+-- Inserting data into ADMINISTRATORS table
+
+-- (INSERT INTO ADMINISTRATORS ...)
+
+
+-- Inserting data into CUSTOMERS table
+
+-- (INSERT INTO CUSTOMERS ...)
+
+
+-- Inserting data into PURCHASES table
+
+-- (INSERT INTO PURCHASES ...)
+
+
+-- Inserting data into PROMOTIONS table
+
+-- (INSERT INTO PROMOTIONS ...)
+
+
+-- Inserting data into INVENTORY table
+
+-- (INSERT INTO INVENTORY ...)
+
+
+-- Inserting data into REVIEWS table
+
+-- (INSERT INTO REVIEWS ...)
+
+
+-- Inserting data into PurchaseDetailReferences table
+
+-- (INSERT INTO PurchaseDetailReferences ...)
+
+
+-- Inserting data into PurchaseItemDetails table
+
+-- (INSERT INTO PurchaseItemDetails ...)
+
